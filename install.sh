d8c9e56ebb3beddad5fb14704be7c65c453263a9
Stuart Axon <stuaxo2@yahoo.com>
2017-11-22 23:12:38 +0000

    Initial commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Install
+
+```bash
+git clone git@github.com:stuaxo/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles
+./install.sh
+```
+
+## Add a new dotfile
+
+```bash
+dotfiles --add ~/.vim
+```
+
+More: https://github.com/stuaxo/dotfiles
+
